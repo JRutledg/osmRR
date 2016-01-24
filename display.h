7eaace69ba71e2c43da1aed1541dc94a3ab11b85
Joe <joe.rutledge@gmail.com>
2016-01-24 12:05:08 +0000

    Adds comments.

--- display.h
+++ display.h
@@ -1,6 +1,8 @@
 //
 // Created by joe on 17/01/16.
 //
+// Display related classes
+// Intended to abstract the display type (X, Windows, PDF, etc.) from the rest of the rendering
 
 #ifndef OSMMAPPER_DISPLAY_H
 #define OSMMAPPER_DISPLAY_H
@@ -19,35 +21,43 @@
 
 namespace osmMapper {
 
+    /*! Base for all rendered output*/
     class display {
     public:
+        //! CTOR
+        //! \param x Initial width of display in display units
+        //! \param y Initial height of display in display units
         display(int &x, int &y) : m_width(x), m_height(y) {}
 
+        //! DTOR
         virtual ~display(){}
 
+        //! Sets the display to fullsize/fullscreen
         virtual void fullSize(bool full= true){}
 
-        virtual void closeX11CairoSurface(cairo_surface_t *sfc){}
-
+        //! Gets the current width of the display in display units
         virtual int width(void) const {return m_width;}
 
+        //! Gets the current height of the display in display units
         virtual int height(void) const {return m_height;}
 
+        //! Renders a line on the display
+        //! \param from The position to draw line from
+        //! \param to The position to draw the line to
         virtual void drawLine(const screenPoint &from, const screenPoint &to){};
 
     protected:
-        int m_width;
-        int m_height;
+        int m_width;    //!< Width of display in display units
+        int m_height;   //!< Height of display in display units
     };
 
+    /*! A display that operates via XLib */
     class xDisplay : public display {
     public:
         xDisplay(int &x, int &y) : display(x, y) {createX11CairoSurface(&x, &y);}
         ~xDisplay(){}
 
         void fullSize(bool full= true){}
-
-        void closeX11CairoSurface(cairo_surface_t *sfc);
 
         int width(void) const {return m_width;}
 
@@ -56,11 +66,21 @@ namespace osmMapper {
         void drawLine(const screenPoint &from, const screenPoint &to);
 
     private:
+        //! Creates an X11 Cairo surface, of x and y in size or fullscreen if x and y are 0
+        //! \param[in,out] x The initial width of the surface, returns the actual width of the surface
+        //! \param[in,out] y The intial height of the surface, returns the actual height of the surface
         void createX11CairoSurface(int *x, int *y);
+
+        //! Frees up an X11 Cairo surface
+        void closeX11CairoSurface(cairo_surface_t *sfc);
+
         void fullscreen(Display *dpy, Window win);
+
+        //! Temporary. This needs to move somewhere else.
         screenPoint compassPointToScreenPoint(const compassPoint &inPoint);
-        Cairo::RefPtr<Cairo::XlibSurface> m_surface;
-        Cairo::RefPtr<Cairo::Context> m_context;
+
+        Cairo::RefPtr<Cairo::XlibSurface> m_surface;    //!< CairoMM reference to the surface
+        Cairo::RefPtr<Cairo::Context> m_context;        //!< CairoMM reference to the draw context
     };
 
 } // namespace osmMapper
